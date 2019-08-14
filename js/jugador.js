e38f0522c812b6e3ca68ca9fdd42fcc5493730af
Joji Depietri <jojidepietri@Joji.local>
2019-08-14 17:28:09 -0300

    Guía 2 completa y funcionado

--- js/jugador.js
+++ js/jugador.js
@@ -10,6 +10,33 @@ var Jugador = {
   alto: 30,
   velocidad: 10,
   vidas: 5,
+  mover: function(tecla, movX, movY) {
+    if (tecla == 'izq' || tecla == 'der') {
+      this.ancho = 30;
+      this.alto = 15;
+    } else {
+      this.ancho = 15;
+      this.alto = 30;
+    };
+
+    switch (tecla) {
+      case 'izq': this.sprite = 'imagenes/auto_rojo_izquierda.png';
+      break;
+      case 'der': this.sprite = 'imagenes/auto_rojo_derecha.png';
+      break;
+      case 'arriba': this.sprite = 'imagenes/auto_rojo_arriba.png';
+      break;
+      default: this.sprite = 'imagenes/auto_rojo_abajo.png';
+      break;
+    };
+
+    this.x += movX;
+    this.y += movY;
+  },
+  perderVidas: function() {
+    this.vidas --;
+    console.log('El jugador perdió vidas');
+  }
   // Hay que agregar lo que falte al jugador: movimientos, perdida de vidas,
   // y todo lo que haga falta para que cumpla con sus responsabilidades
 
